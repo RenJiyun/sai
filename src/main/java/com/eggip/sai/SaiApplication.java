7486259ab02578d551f63a607dae06205a47f029
RenJiyun <wozaijixu@msn.cn>
2019-05-14 21:25:39 +0800

    首次构建

---
+++ src/main/java/com/eggip/sai/SaiApplication.java
@@ -0,0 +1,13 @@
+package com.eggip.sai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SaiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SaiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/eggip/sai/SaiApplicationTests.java
@@ -0,0 +1,16 @@
+package com.eggip.sai;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SaiApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
